de2a6b992fdf0b01d4170651f26e35f7e080caac
rhos0129 <rhos0129@gmail.com>
2023-12-06 02:06:23 +0900

    S02 API

--- src/main/java/com/example/springintro/controller/HelloController.java
+++ src/main/java/com/example/springintro/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +20,33 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    // String을 반환하는 API
+    // 실행 후 소스 확인 시 데이터를 문자 그대로 반환
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    // Object를 반환하는 API (기본방식)
+    // 실행 후 소스 확인 시 Json으로 반환
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 
 }
