781561200c4f35a96f6ea4b22ef8cfeea56e75d2
rhos0129 <rhos0129@gmail.com>
2023-12-06 02:14:11 +0900

    S05 회원 웹 기능 - 등록

--- src/main/java/com/example/springintro/controller/MemberController.java
+++ src/main/java/com/example/springintro/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.example.springintro.controller;
 
+import com.example.springintro.domain.Member;
 import com.example.springintro.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // 스프링 빈으로 등록
 public class MemberController {
@@ -12,6 +15,22 @@ public class MemberController {
     @Autowired // 스프링 빈으로 등록되어 있는 객체를 찾아서 주입
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+
+    // 등록 화면
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    // 등록 기능
+    @PostMapping(value = "/members/new")
+    public String create(MemebrForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/"; // 홈 화면으로 리다이렉트
     }
 
 }
