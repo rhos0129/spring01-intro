433a099fa032a563f1d3aa89ac407db654e5cbb1
rhos0129 <rhos0129@gmail.com>
2023-12-06 02:07:05 +0900

    S03 회원 도메인과 리포지토리 만들기

---
+++ src/main/java/com/example/springintro/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.example.springintro.repository;
+
+import com.example.springintro.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
---
+++ src/main/java/com/example/springintro/repository/MemoryMemberRepository.java
@@ -0,0 +1,42 @@
+package com.example.springintro.repository;
+
+import com.example.springintro.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    // 일단 Map<Long, Member>에 데이터 저장, id는 Long 사용
+    // 실무에서는 동시성 문제를 고려하여 ConcurrentHashMap, AtomicLong을 사용
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+
+}
