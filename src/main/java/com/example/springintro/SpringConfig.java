c4906ae33cb645f0558b77ba622303cca7eceaeb
rhos0129 <rhos0129@gmail.com>
2023-12-06 02:21:15 +0900

    S06 스프링 JdbcTemplate

--- src/main/java/com/example/springintro/SpringConfig.java
+++ src/main/java/com/example/springintro/SpringConfig.java
@@ -1,6 +1,7 @@
 package com.example.springintro;
 
 import com.example.springintro.repository.JdbcMemberRepository;
+import com.example.springintro.repository.JdbcTemplateMemberRepository;
 import com.example.springintro.repository.MemberRepository;
 import com.example.springintro.repository.MemoryMemberRepository;
 import com.example.springintro.service.MemberService;
@@ -26,7 +27,8 @@ public class SpringConfig {
     @Bean // 스프링 빈으로 수동 등록
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource); // 기존 리포지토리 구현체를 Jdbc리포지토리로 변경 (개방-폐쇄 원칙 OCP)
+//        return new JdbcMemberRepository(dataSource); // 기존 리포지토리 구현체를 Jdbc리포지토리로 변경 (개방-폐쇄 원칙 OCP)
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
